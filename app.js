e09c3ad3df82f8d613c91d4dee8bc7c9d97e6b81
cnishaanntt <cnishaanntt@gmail.com>
2018-06-24 11:58:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -120,9 +120,10 @@ function handleMessage(sender_psid, received_message) {
                   }
                 ]
             }  
-       // } else if(received_message.text==="Search"){
+        } else if(received_message.text==="Search"){
             
-            //function search_operation(sender_psid);
+            function search_operation(sender_psid);
+            continue;
         } else {
              response = {
               "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
